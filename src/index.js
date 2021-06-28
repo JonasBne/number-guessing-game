3d3fc7baae0fe07df6341b542a9b732b016cebbb
Jonas Boone <jonas.boone@student.kdg.be>
2021-06-28 21:56:56 +0200

    Added function to generate random number

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const computerScoreBoard = document.getElementById("computer-score");
 // Define game values
 let min = 1;
 let max = 10;
-let winningNumber = 2;
+let winningNumber = generateRandomNumber(min, max);
 let guessesLeft = 3;
 let playerScore = 0;
 let computerScore = 0;
@@ -44,7 +44,7 @@ guessBtn.addEventListener("click", () => {
             // Add winning message & border
             setMessageAndBorder(`The winning number is ${winningNumber}. You win!`, "green");
             // Update scoreboard
-            updateScoreBoard("win");
+            updateScoreBoard("won");
             // Disable input field and start new game
             disableInput();
     } else {
@@ -55,7 +55,7 @@ guessBtn.addEventListener("click", () => {
                 // Add error message
                 setMessageAndBorder(`You have ${guessesLeft} guesses left, you lost. The correct number was ${winningNumber}.`, "red");
                 // Update scoreboard
-                updateScoreBoard();
+                updateScoreBoard("lost");
                 // Disable input field and start new game
                 disableInput();
         } else {
@@ -89,11 +89,11 @@ function disableInput() {
 }
 
 // Scoreboard
-function updateScoreBoard(win) {
-    if (win) {
+function updateScoreBoard(gameStatus) {
+    if (gameStatus === "won") {
         playerScore += 1;
         playerScoreBoard.innerText = playerScore;
-    } else {
+    } else if (gameStatus === "lost") {
         computerScore += 1;
         computerScoreBoard.innerText = computerScore;
     }
@@ -101,6 +101,9 @@ function updateScoreBoard(win) {
 
 // Start new game
 function startNewGame() {
+    //todo: reload page to generate a new random number
+    // Reset guesses left
+    guessesLeft = 3;
     // Clear input
     inputNumber.value = "";
     // Reset message
@@ -113,3 +116,10 @@ function startNewGame() {
     newGameBtn.classList.add("hidden");
     guessBtn.classList.remove("hidden");
 }
+
+// Generate a winning number
+function generateRandomNumber(min, max) {
+    // Return to store in variable
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
