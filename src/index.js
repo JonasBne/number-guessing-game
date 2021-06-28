fb14f677b99f992084d3018b5c14d8c0ca43c08f
Jonas Boone <jonas.boone@student.kdg.be>
2021-06-28 21:15:09 +0200

    Added calculations to update scoreboard

--- src/index.js
+++ src/index.js
@@ -11,13 +11,16 @@ const inputNumber = document.getElementById("input-number");
 const guessBtn = document.getElementById("guess-btn");
 const newGameBtn = document.getElementById("new-game-btn");
 const message = document.getElementById("message");
-const gameContainer = document.getElementById("game");
+const playerScoreBoard = document.getElementById("player-score");
+const computerScoreBoard = document.getElementById("computer-score");
 
 // Define game values
 let min = 1;
 let max = 10;
 let winningNumber = 2;
 let guessesLeft = 3;
+let playerScore = 0;
+let computerScore = 0;
 
 // Assign UI min and max number
 minNumber.innerText = min;
@@ -27,34 +30,50 @@ maxNumber.innerText = max;
 guessBtn.addEventListener("click", () => {
     // Parse from string to number
     let guessedNumber = parseInt(inputNumber.value);
+
     // Validate input
     if (isNaN(guessedNumber) || guessedNumber < min || guessedNumber > max) {
         // Add error message
         setMessageAndBorder(`Only numbers between ${min} and ${max} are allowed!`, "red");
     }
+
     // Check if won
     if (guessedNumber === winningNumber) {
-        // Add winning message
-        setMessageAndBorder(`The winning number is ${winningNumber}. You win!`, "green");
-        // Add border color
-        inputNumber.style.borderColor = "green";
-        // Disable input field
-        inputNumber.setAttribute("readonly", true);
-    } else {
-        // Subtract one guess from the remaining guesses
-        guessesLeft -= 1;
-        // Add error message
-        setMessageAndBorder(`Wrong guess. You have ${guessesLeft} guess(es) left.`, "red");
-        // Check number of guesses
-        if (guessesLeft === 0) {
-            // Add error message
-            setMessageAndBorder(`You have ${guessesLeft} guesses left. Start over.`, "red");
+        // Winning case
+
+            // Add winning message
+            setMessageAndBorder(`The winning number is ${winningNumber}. You win!`, "green");
+            // Add border color
+            inputNumber.style.borderColor = "green";
+            // Update scoreboard
+            playerScore += 1;
+            playerScoreBoard.innerText = playerScore;
             // Disable input field
             inputNumber.setAttribute("readonly", true);
             // Hide button
             guessBtn.classList.add("hidden");
             // Show button to start over
             newGameBtn.classList.remove("hidden");
+    } else {
+        // Losing case
+
+            // Subtract one guess from the remaining guesses
+            guessesLeft -= 1;
+            // Add error message
+            setMessageAndBorder(`Wrong guess. You have ${guessesLeft} guess(es) left.`, "red");
+            // Check number of guesses
+            if (guessesLeft === 0) {
+                // Add error message
+                setMessageAndBorder(`You have ${guessesLeft} guesses left. Start over.`, "red");
+                // Update scoreboard
+                computerScore += 1;
+                computerScoreBoard.innerText = computerScore;
+                // Disable input field
+                inputNumber.setAttribute("readonly", true);
+                // Hide button
+                guessBtn.classList.add("hidden");
+                // Show button to start over
+                newGameBtn.classList.remove("hidden");
         } else {
 
         }
